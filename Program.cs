69245b57aa69884d07ebbcc87ccfa3eaf3852d2d
daniel-ldg <daniel.ldg@icloud.com>
2018-08-21 16:51:46 -0500

    Commit inicial. Se agrega gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Actividad001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daniel");
+        }
+    }
+}
