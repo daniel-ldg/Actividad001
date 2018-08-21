125de8825d5b534debc359d8b800e7148723acbb
daniel-ldg <daniel.ldg@icloud.com>
2018-08-21 16:57:05 -0500

    Program.cs modificado

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Actividad001
         static void Main(string[] args)
         {
             Console.WriteLine("Daniel");
+            Console.WriteLine("gvuebhcinjxomk");
         }
     }
 }
